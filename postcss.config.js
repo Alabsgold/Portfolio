b574a39d74368321d6b26460a004d4e4906e3516
Alabsgold <155645248+Alabsgold@users.noreply.github.com>
2025-11-04 11:49:46 +0000

    updated postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+/** @type {import('postcss').Config} */
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    // 1. We use the official Tailwind CSS PostCSS plugin now:
+    '@tailwindcss/postcss': {}, 
+    
+    // 2. Autoprefixer ensures your CSS works across different browsers:
+    'autoprefixer': {},
   },
 }
